684ffb9cc519fe872e3b8e839d8e328af13a0527
tooscco <aladejuyigbetosin7@gmail.com>
2024-05-03 15:33:55 +0100

    still working

---
+++ script.js
@@ -0,0 +1,98 @@
+document.title="Dashboard Side Menu"
+
+const dashboard = () =>{
+    window.location.href="index.html"
+}
+window.dashboard=dashboard
+
+const myProfile = () =>{
+    window.location.href="myProfile.html"
+}
+window.myProfile=myProfile
+
+const changePassword = () =>{
+    window.location.href="changePassword.html"
+}
+window.changePassword=changePassword
+  
+const proDocument = () =>{
+    window.location.href="document.html"
+}
+window.proDocument=proDocument
+
+const courseRegistration = () =>{
+    window.location.href="courseRegistration.html"
+}
+window.courseRegistration=courseRegistration
+
+const resource = () =>{
+    window.location.href="resource.html"
+}
+window.resource=resource
+
+const registrationHistory = () =>{
+    window.location.href="registrationHistory.html"
+}
+window.registrationHistory=registrationHistory
+
+const semesterResult = () =>{
+    window.location.href="semesterResult.html"
+}
+window.semesterResult=semesterResult
+
+const viewTimetable = () =>{
+    window.location.href="viewTimetable.html"
+}
+window.viewTimetable=viewTimetable
+
+const payTuition = () =>{
+    window.location.href="payTuition.html"
+}
+window.payTuition=payTuition
+
+const paymentHistory = () =>{
+    window.location.href="paymentHistory.html"
+}
+window.paymentHistory=paymentHistory
+
+const transactionHistory = () =>{
+    window.location.href="transactionHistory.html"
+}
+window.transactionHistory=transactionHistory
+
+const noticeBoard = () =>{
+    window.location.href="noticeBoard.html"
+}
+window.noticeBoard=noticeBoard
+
+ const accomodation = () =>{
+    window.location.href="accomodation.html"
+ }
+ window.accomodation=accomodation
+
+ const electionButton = () =>{
+    window.location.href="election.html"
+ }
+ window.electionButton=electionButton
+
+ const certificate = () =>{
+    window.location.href="certificate.html"
+ }
+ window.certificate=certificate
+
+ const help = () =>{
+    window.location.href="help.html"
+ }
+window.help=help
+
+const logout = () =>{
+    window.location.href=""
+}
+window.logout=logout
+
+// const menuClick =document.getElementById('menuClick')
+// const dashBoardDiv = document.getElementById('dashBoardDiv')
+
+// menuClick.addEventListener('click', function() {
+//     dashBoardDiv.classList.add('hidden')
+// })
